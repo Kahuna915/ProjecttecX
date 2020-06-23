3c8922bf6ae4df7ba47d94e72b005c78b03b6847
Kahuna915 <62860262+Kahuna915@users.noreply.github.com>
2020-06-23 17:46:42 -0400

    Add files via upload

---
+++ ssh push file 2.ps1
@@ -0,0 +1,9 @@
+﻿cd 'C:\Users\noahs\OneDrive\Documents\PowerShell practicew'
+$file = Read-Host -Prompt "What is the file name you would like to push?"
+$password = Read-Host -AsSecureString "Enter the password for server"
+$password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+$servername = "52.147.167.157:"
+$username = "noah"
+$rdir = "/home/noah"
+echo y | pscp.exe -P '22' -pw $password $file $username@$servername$rdir
+
